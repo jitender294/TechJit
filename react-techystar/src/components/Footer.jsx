bb8ab4434547e08c710a31851ff6ba93abc94cc0
Jitender Sharma <js903172@gmail.com>
2024-10-26 18:00:08 +0530

    first

---
+++ react-techystar/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechJit</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/@Jitender294" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/jitender_sharma___/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Jitender294" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
